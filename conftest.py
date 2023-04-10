fb67a5e017bcf175a4b46ac7cbe54e50c93546d1
mdesyatnikova <mdesyatnikova@gmail.com>
2023-04-10 15:45:53 +0300

    add config file target
    опции вынесены в конфигурационный файл target

---
+++ conftest.py
@@ -0,0 +1,37 @@
+import json
+import pytest
+import os.path
+from fixture.application import Application
+
+fixture = None
+target = None
+
+
+@pytest.fixture
+def app(request):
+    global fixture
+    global target
+    browser = request.config.getoption("--browser")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["base_url"])
+    fixture.session.ensure_login(user_name=target["user_name"], password=target["password"])
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
+    return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--target", action="store", default="target.json")
